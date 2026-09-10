401d8b9832715b01e3becaa3874d000939870171
Gabriel Dahlem <gabriel.dahlem@gmail.com>
2026-09-10 15:15:26 -0300

    linha com tempo com dev atual

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 
         const nodes = document.querySelectorAll('.node-wrapper');
         const cards = document.querySelectorAll('.card');
@@ -9,11 +10,23 @@
             cards.forEach(card => card.classList.remove('active'));
 
            
+=======
+const nodes = document.querySelectorAll('.node-wrapper');
+        const cards = document.querySelectorAll('.card');
+
+        function setActive(index) {
+            nodes.forEach(node => node.classList.remove('active'));
+            cards.forEach(card => card.classList.remove('active'));
+
+>>>>>>> Stashed changes
             if (nodes[index]) nodes[index].classList.add('active');
             if (cards[index]) cards[index].classList.add('active');
         }
 
+<<<<<<< Updated upstream
        
+=======
+>>>>>>> Stashed changes
         nodes.forEach(node => {
             node.addEventListener('click', () => {
                 const index = node.getAttribute('data-index');
@@ -21,7 +34,10 @@
             });
         });
 
+<<<<<<< Updated upstream
         
+=======
+>>>>>>> Stashed changes
         cards.forEach(card => {
             card.addEventListener('click', () => {
                 const index = card.getAttribute('data-index');
