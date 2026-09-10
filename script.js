0d20e3a233ecd43cbbd4f99355502d4db7bf37cc
FABIO RAFAEL ONOFRE <202512813@academico.unilasalle.edu.br>
2026-09-10 16:19:48 -0300

    Estrutura do cabeçalho

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+  header.classList.toggle('scrolled', window.scrollY > 50);
+});
+
 const nodes = document.querySelectorAll('.node-wrapper');
         const cards = document.querySelectorAll('.card');
 
